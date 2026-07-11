fe829ee8d041b1bb5bed087abe4beaf967a0ce45
mradul-gupta18 <mradul.gupta187@gmail.com>
2026-07-11 18:08:20 +0530

    Add files via upload

---
+++ 25ESKCA148/DAY-10 , 11 & 12/dashboardverticalcontent.php
@@ -0,0 +1,26 @@
+<div class="col-md-4">
+        <!-- Account Details Card -->
+            <div class="card shadow rounded-4 card-body text-center">
+                
+                    <h3 class="fw-bold mb-3">
+                        Account Settings
+                    </h3>
+
+                    <p class="text-muted">
+                        Manage your profile and account security.
+                    </p>
+
+                    <hr>
+
+                    <a href="updateProfile.php"
+                        class="btn btn-success w-100 my-2 rounded-pill">
+                        Update Profile
+                    </a>
+
+                    <a href="updatePassword.php"
+                        class="btn btn-primary w-100 my-2 rounded-pill">
+                        Change Password
+                </a>
+        </div>
+    </div>
+</div>
